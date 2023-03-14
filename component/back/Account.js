ad9dc6fa7829ce5013483f298dde8367e03da85c
Arotiana <arotiana4612@gmail.com>
2023-03-14 15:06:33 +0300

    Image  fetching

--- component/back/Account.js
+++ component/back/Account.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import { useUser, useSupabaseClient } from '@supabase/auth-helpers-react'
+import Avatar from "./Avatar"
 
 export default function Account({ session }) {
   const supabase = useSupabaseClient()
